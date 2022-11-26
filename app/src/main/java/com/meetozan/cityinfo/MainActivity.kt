f2523512251cd0a2ea49cf155207e975c879aafd
Mert Ozan Kahraman <mozankahraman69@gmail.com>
2022-11-26 13:56:31 +0300

    Card, Dialog and Toolbar added

--- app/src/main/java/com/meetozan/cityinfo/MainActivity.kt
+++ app/src/main/java/com/meetozan/cityinfo/MainActivity.kt
@@ -2,10 +2,44 @@ package com.meetozan.cityinfo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var adapter: MainAdapter
+    private lateinit var rv: RecyclerView
+    val db = Firebase.firestore.collection("cities")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        getLiveCity()
+    }
+
+    private fun getLiveCity() {
+        db.addSnapshotListener { querySnapshot, firebaseFirestoreException ->
+            firebaseFirestoreException?.let {
+                Toast.makeText(this, it.message, Toast.LENGTH_LONG).show()
+                return@addSnapshotListener
+            }
+            querySnapshot?.let {
+                val cityList: ArrayList<City> = ArrayList()
+                for (document in it) {
+                    val city = document.toObject<City>()
+                    cityList.add(city)
+                }
+                rv = findViewById(R.id.mainRv)
+                rv.layoutManager = LinearLayoutManager(this@MainActivity)
+                rv.setHasFixedSize(true)
+
+                adapter = MainAdapter(cityList,this@MainActivity)
+                rv.adapter = adapter
+            }
+        }
     }
 }
--- app/src/main/java/com/meetozan/cityinfo/MainAdapter.kt
+++ app/src/main/java/com/meetozan/cityinfo/MainAdapter.kt
@@ -1,18 +1,26 @@
 package com.meetozan.cityinfo
 
+import android.app.AlertDialog
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.meetozan.cityinfo.databinding.CityCardBinding
 
-class MainAdapter(private val cityList: List<City>) : RecyclerView.Adapter<MainAdapter.ViewHolder>(){
+class MainAdapter(private val cityList: List<City>, private val context: Context) :
+    RecyclerView.Adapter<MainAdapter.ViewHolder>() {
 
-    class ViewHolder(binding: CityCardBinding):RecyclerView.ViewHolder(binding.root){
-        val cityBinding : CityCardBinding = binding
+    private lateinit var mContext: Context
+
+    class ViewHolder(binding: CityCardBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        val cityBinding: CityCardBinding = binding
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val binding = CityCardBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        val binding = CityCardBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ViewHolder(binding)
     }
 
@@ -20,6 +28,36 @@ class MainAdapter(private val cityList: List<City>) : RecyclerView.Adapter<MainA
         val city = cityList[position]
         holder.cityBinding.cityCard = city
 
+
+        holder.cityBinding.cardCity.setOnClickListener {
+            mContext = context
+            val dialog = LayoutInflater.from(mContext).inflate(R.layout.custom_dialog, null)
+            val builder = AlertDialog.Builder(mContext).setView(dialog).show()
+            builder.setCancelable(true)
+
+            val txtName = dialog.findViewById<TextView>(R.id.alertName)
+            txtName.text = city.name
+
+            val txtNumber = dialog.findViewById<TextView>(R.id.alertNumber)
+            txtNumber.text = city.number
+
+            val txtPopulation = dialog.findViewById<TextView>(R.id.alertPopulation)
+            txtPopulation.text = city.population.toString()
+
+            val txtRegion = dialog.findViewById<TextView>(R.id.alertRegion)
+            txtRegion.text = city.region
+
+            val txtMajor = dialog.findViewById<TextView>(R.id.alertMajor)
+            txtMajor.text = city.major
+
+            val txtFood = dialog.findViewById<TextView>(R.id.alertFood)
+            txtFood.text = city.food
+
+            val imageBack = dialog.findViewById<ImageView>(R.id.imageBack)
+            imageBack.setOnClickListener {
+                builder.dismiss()
+            }
+        }
     }
 
     override fun getItemCount(): Int = cityList.size
